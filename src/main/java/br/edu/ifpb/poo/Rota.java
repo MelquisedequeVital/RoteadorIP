12058f4d5bec7f341921cafabcdb87411e011878
Melquisedeque <melquisedequevital@gmail.com>
2025-11-26 21:27:36 -0300

    Adicionando Rota

---
+++ src/main/java/br/edu/ifpb/poo/Rota.java
@@ -0,0 +1,34 @@
+package br.edu.ifpb.poo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+public class Rota {
+    private int[] destinationIp;
+    private int[] gateway;
+    private int[] mask;
+    private String netInterface;
+
+    public Rota(int[] destinationIp, int[] gateway, int[] mask, String netInterface) {
+
+        if (destinationIp.length != 4) {
+            throw new IllegalArgumentException("O array destinationIp deve conter exatamente 4 elementos (formato IPv4). Tamanho atual: " + destinationIp.length);
+        }
+        
+        if (gateway.length != 4) {
+            throw new IllegalArgumentException("O array gateway deve conter exatamente 4 elementos (formato IPv4). Tamanho atual: " + gateway.length);
+        }
+        
+        if (mask.length != 4) {
+            throw new IllegalArgumentException("O array mask deve conter exatamente 4 elementos (formato IPv4). Tamanho atual: " + mask.length);
+        }
+
+        this.destinationIp = destinationIp;
+        this.gateway = gateway;
+        this.mask = mask;
+        this.netInterface = netInterface;
+    }
+}
